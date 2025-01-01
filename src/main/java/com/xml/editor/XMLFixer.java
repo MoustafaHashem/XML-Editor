c854d8b5a0a9d17cda91242b8e9ea0500633da2a
AshrafByte <ahmed.ashraf2827@gmail.com>
2025-01-01 02:57:30 +0200

    Fix bug in XMLFixer that produce incorrect output

--- src/main/java/com/xml/editor/XMLFixer.java
+++ src/main/java/com/xml/editor/XMLFixer.java
@@ -1,5 +1,4 @@
 package com.xml.editor;
-
 import java.util.*;
 /**
  * A utility class for fixing XML content by ensuring that all tags are properly opened and closed.
@@ -17,7 +16,6 @@ public class XMLFixer
 {
     private final List<String> lines;
     private final XMLTagsTracker openTags;
-    private final Stack<String> bodyStack;
     private final List<String> fixedLines;
 
     /**
@@ -29,7 +27,6 @@ public class XMLFixer
     {
         this.lines = lines;
         this.openTags = new XMLTagsTracker();
-        this.bodyStack = new Stack<>();
         fixedLines = new ArrayList<>();
     }
 
@@ -44,6 +41,13 @@ public class XMLFixer
      */
     public List<String> fix()
     {
+        processLines();
+        addMissingClosingTag();
+        return fixedLines;
+    }
+
+    private void processLines()
+    {
         for (String line : lines)
         {
             String trimmedLine = line.trim();
@@ -51,89 +55,61 @@ public class XMLFixer
                 continue; // Skip empty lines
 
             List<String> tokens = XMLTagUtils.splitTagsAndContent(trimmedLine);
-            StringBuilder fixedLine = new StringBuilder();
-            boolean bodyEnclosed = true;
-
-            for (String token : tokens)
-            {
-                if (XMLTagUtils.isOpenTag(token))
-                {
-                    if (openTags.isRepeatedTag(token))
-                    {
-                        openTags.popTag();
-                        bodyEnclosed = true;
-                        fixedLine.append(XMLTagUtils.createClosingTag(token));
-                    }
-                    else if (openTags.isTagRepeatedOnce(token))
-                    {
-                        fixedLine.append(XMLTagUtils.createClosingTag(openTags.popTag()));
-                    }
-                    else
-                    {
-                        openTags.pushTag(token);
-                        fixedLine.append(token);
-                    }
-                }
-                else if (XMLTagUtils.isCloseTag(token))
-                {
-                    if (openTags.isTopTagMatches(token))
-                    {
-                        bodyEnclosed = true;
-                        openTags.popTag();
-                        fixedLine.append(token);
-                    }
-                    else
-                    {
-                        if (!openTags.isEmpty())
-                        {
-                            String openTag = XMLTagUtils.createOpeningTag(token);
-                            if (openTags.isTagPresent(openTag))
-                                continue;
-
-                            String closeTag = XMLTagUtils.createClosingTag(openTags.popTag());
-                            bodyEnclosed = true;
-                            fixedLine.append(closeTag);
-                            fixedLines.add(fixedLine.toString());
-                            fixedLine = new StringBuilder();
-                            if (openTags.isTopTagMatches(token))
-                            {
-                                openTags.popTag();
-                                fixedLine.append(token);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    // It's body content, so append it and immediately close the last open tag
-                    fixedLine.append(token);
-                    bodyEnclosed = false;
-                }
-            }
-
-            if (!bodyEnclosed)
-            {
-                if (!openTags.isEmpty())
-                {
-                    String closeTag = XMLTagUtils.createClosingTag(openTags.popTag());
-                    fixedLine.append(closeTag);
-                }
-            }
-
-            if (!fixedLine.isEmpty())
-                fixedLines.add(fixedLine.toString());  // Only add non-empty lines
+            processTokens(tokens);
         }
+    }
 
-        // Add missing closing tags for remaining open tags
+    private void processTokens(List <String> tokens)
+    {
+        for (String token : tokens)
+        {
+            if (XMLTagUtils.isOpenTag(token))
+                handleOpenTag(token);
+
+            else if (XMLTagUtils.isCloseTag(token))
+                handleCloseTag(token);
+
+            else
+                handleBody(token);
+        }
+    }
+
+    private void handleOpenTag(String tag)
+    {
+        openTags.pushTag(tag);
+        fixedLines.add(tag);
+    }
+    private void handleCloseTag(String tag)
+    {
+        if (openTags.isTopTagMatches(tag))
+        {
+            fixedLines.add(tag);
+            openTags.popTag();
+        }
+        else
+        {
+            if (openTags.isTagPresent(XMLTagUtils.createOpeningTag(tag)))
+                fixedLines.add(XMLTagUtils.createClosingTag(openTags.popTag()));
+            else
+            {
+                fixedLines.add(XMLTagUtils.createOpeningTag(tag));
+                fixedLines.add(tag);
+            }
+        }
+    }
+
+    private void handleBody(String content)
+    {
+        fixedLines.add(content);
+    }
+
+    private void addMissingClosingTag()
+    {
         while (!openTags.isEmpty())
         {
             String closeTag = XMLTagUtils.createClosingTag(openTags.popTag());
             fixedLines.add(closeTag); // Add each missing closing tag on a new line
             System.out.println("Missing closing tag added: " + closeTag);
         }
-
-        return fixedLines;
     }
-
-
 }
